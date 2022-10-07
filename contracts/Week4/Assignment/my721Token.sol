b9d49d286c1fcd2d180844caea7a4b183e9f181a
Rohit Suryawanshi <47253310+suryaadev@users.noreply.github.com>
2022-10-07 23:08:33 +0530

    Update my721Token.sol

--- contracts/Week4/Assignment/my721Token.sol
+++ contracts/Week4/Assignment/my721Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.7;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
